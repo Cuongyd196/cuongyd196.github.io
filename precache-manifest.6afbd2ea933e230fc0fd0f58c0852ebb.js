686887f22b32e3b08162a30e4ecaff1c189599d1
Cuongyd196 <62423513+Cuongyd196@users.noreply.github.com>
2021-09-06 15:54:19 +0700

    Updates

---
+++ precache-manifest.6afbd2ea933e230fc0fd0f58c0852ebb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "632b2d2d556f4d6be5eb33f91dc68e61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05053ee18d3f38214482",
+    "url": "/static/css/2.c705ca7a.chunk.css"
+  },
+  {
+    "revision": "c30fdb7a84425cb8a930",
+    "url": "/static/css/main.9e70ebcb.chunk.css"
+  },
+  {
+    "revision": "05053ee18d3f38214482",
+    "url": "/static/js/2.96a8a70b.chunk.js"
+  },
+  {
+    "revision": "c095ddabaa80286a37df2a6fd5ab6041",
+    "url": "/static/js/2.96a8a70b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a22ae97398a1b4a4ac8b",
+    "url": "/static/js/3.43b8db87.chunk.js"
+  },
+  {
+    "revision": "238529e19a7a6202f45a",
+    "url": "/static/js/4.986abe08.chunk.js"
+  },
+  {
+    "revision": "c30fdb7a84425cb8a930",
+    "url": "/static/js/main.007c0381.chunk.js"
+  },
+  {
+    "revision": "24fdd03a9fd11171b241",
+    "url": "/static/js/runtime-main.a5bce743.js"
+  },
+  {
+    "revision": "855fa67997139e643fdc342d4607bc8a",
+    "url": "/static/media/avt4.855fa679.jpg"
+  },
+  {
+    "revision": "625dd3adb45190a06a6d27854bdd0266",
+    "url": "/static/media/portrait_bg.625dd3ad.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e04df9b36eb4c51bfb5a8f1f18d6ca63.js"
+  "/precache-manifest.6afbd2ea933e230fc0fd0f58c0852ebb.js"
 );
 
 self.addEventListener('message', (event) => {
