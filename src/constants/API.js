8a7b5e2246b564c6c704f53b1983a5593dd785af
Cuongyd196 <62423513+Cuongyd196@users.noreply.github.com>
2021-09-12 22:26:19 +0700

    #update api

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,6 +1,6 @@
-// export const HOST_API = 'https://apimyblog.herokuapp.com'
+export const HOST_API = 'https://apimyblog.herokuapp.com'
 
-export const HOST_API = 'http://localhost:3333'
+// export const HOST_API = 'http://localhost:3333'
 
 
 export const API = {
